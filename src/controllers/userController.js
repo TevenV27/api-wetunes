39c716f2d5684acaed6139d5ee7c165e2ba4e315
TevenV27 <victoria.kevin@correounivalle.edu.co>
2023-10-01 20:52:32 -0500

    fix return login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -23,7 +23,7 @@ export const login = async (req, res) => {
 
         const token = jwt.sign({ id: user._id }, jwtSecret, { expiresIn: '7d' });
 
-        res.status(200).json({ token, email: user.email });
+        res.status(200).json({ token, email: user.email, firstname: user.firstname, lastname: user.lastname, avataruser: user.avataruser, role: user.role});
     } catch (error) {
         console.error("Error en el proceso de login:", error);
         res.status(500).json({ error: 'Error del servidor' });
